bf03e43f1cddda58f3604ff4fb375fcc04ac5241
Tariqul Islam Shikdar <tariqulshuvon@gmail.com>
2023-06-22 11:38:22 +0600

    unused imports removed

--- src/main/java/WebApp/Enterprise/Pollima/PollimaApplication.java
+++ src/main/java/WebApp/Enterprise/Pollima/PollimaApplication.java
@@ -2,13 +2,9 @@ package WebApp.Enterprise.Pollima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class PollimaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PollimaApplication.class, args);
 	}
